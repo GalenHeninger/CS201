4f2ec5ea14d285c640801353e55e8bc4e092a0e1
Galen <heninger.galen@gmail.com>
2020-09-01 15:08:20 -0800

    Commit message

---
+++ CS201_HW0/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  CS201_HW0
+//
+//  Created by Galen on 8/30/20.
+//  Copyright © 2020 Galen. All rights reserved.
+//
+
+#include <iostream>
+using std::cout;
+
+int main(int argc, const char * argv[]) {
+    
+    for(int i = 0; i < 5; i++) {
+        cout << "Hello, World\n";
+    }
+    
+    cout << "\nThe fading temple bells calm the waves.\nThe world rocks carefully.\nThe sand draws its constellations in the moonlight.\nThe clocks turn back time to greet the spring\nwhile abandoned dolls in the seaweed dream\nwith open pearl eyes,\namong whales snoring dark frequencies into the night.\n";
+    
+    cout <<"\n  Hyper Light Drifter is a 2D action role-playing game developed by Heart Machine.\nThe game pays homage to 8-bit and 16-bit games, and is considered by its lead\ndeveloper Alex Preston as a combination of The Legend of Zelda: A Link to the Past\nand Diablo.\n";
+    
+    return 0;
+}
