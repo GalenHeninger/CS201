c0bd5fb970b9ecc4c1bc6f04a2f5ced078950ab2
Galen Heninger <heninger.galen@gmail.com>
2020-09-21 15:32:50 -0800

    Edited to answer homework questions

--- CS201_HW2/CS201_HW2/RiceProgram/main.cpp
+++ CS201_HW2/CS201_HW2/RiceProgram/main.cpp
@@ -15,12 +15,28 @@ using std::cin;
 
 int main(int argc, const char * argv[]) {
     
-    int n;
-    cout << "Type a number of rice grains.";
-    cin >> n;
     
-    cout << "The required number of squares for " << n << " rice grains is " ;
-    cout << NumberOfSquaresRequired(n);
+    cout << "\nThe required number of squares for 1000 rice grains is " << NumberOfSquaresRequired(1000);
+    cout << "\nThe required number of squares for 1000000 rice grains is " << NumberOfSquaresRequired(1000000);
+    cout << "\nThe required number of squares for 1000000000 rice grains is " << NumberOfSquaresRequired(1000000000);
+    
+//    int n;
+//    cout << "\nType a number of rice grains.\n";
+//    cin >> n;
+//
+//    cout << "\nThe required number of squares for " << n << " rice grains is " ;
+//    cout << NumberOfSquaresRequired(n);
+    
+    cout << "\nThe number of rice grains needed for the series with 64 terms\nis equal to 2^64 - 1, approximately " << pow(2, 64) - 1;
+    cout << "\nThe largest power for which a value of rice grains can be assigned is...";
+    
+    int n1 = pow(2, 31) - 1;
+    double n2 = pow(2, 1023) - 1;
+    
+    cout << "\nfor an integer variable, 31: " << n1;
+    cout << "\nfor a double variable, 1023:" << n2;
+    
+    
     cout << std::endl;
     
     return 0;
