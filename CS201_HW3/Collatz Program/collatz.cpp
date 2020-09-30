06861142f734aab2ee05f9882f174731030e9312
Galen Heninger <heninger.galen@gmail.com>
2020-09-29 16:29:39 -0800

    Program now ensures that the user input was positive.

--- CS201_HW3/Collatz Program/collatz.cpp
+++ CS201_HW3/Collatz Program/collatz.cpp
@@ -10,7 +10,7 @@
 
 int main(int argc, const char * argv[]) {
     
-    std::cout << "Type an integer.\n";
+    std::cout << "Type a positive integer.\n";
     
     // Collatz function: 3n+1 when n is odd, n/2 when n is even
     
@@ -18,6 +18,10 @@ int main(int argc, const char * argv[]) {
     
     //User input for the initial value of n
     std::cin >> n;
+    while (n<1) {
+        std::cout << "The value of n must be greater than 0.\n";
+        std::cin >> n;
+    }
     std::cout << "Input was " << n;
     std::cout << std::endl;
     
