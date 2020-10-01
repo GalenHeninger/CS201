442ef208d22a8aff16423561a0570f1f5246e1b5
Galen Heninger <heninger.galen@gmail.com>
2020-09-30 17:24:21 -0800

    Began definition for quadratic roots function

---
+++ CS201_HW3/quadratic/quadratic.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  quadratic
+//
+//  Created by Galen on 9/30/20.
+//  Copyright © 2020 Galen. All rights reserved.
+//
+
+#include <iostream>
+
+float quadratic(float a, float b, float c) {
+    
+    
+    
+    return x1, x2;
+    
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
