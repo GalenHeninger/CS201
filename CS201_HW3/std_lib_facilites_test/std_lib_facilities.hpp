3d265632b46c0ee4c809906bab95795cb42d8a4f
Galen Heninger <heninger.galen@gmail.com>
2020-09-30 16:29:41 -0800

    Wrote a function that finds the root of a differentiable function numerically

--- CS201_HW3/std_lib_facilites_test/std_lib_facilities.hpp
+++ CS201_HW3/std_lib_facilites_test/std_lib_facilities.hpp
@@ -27,6 +27,14 @@ using std::endl;
 using std::string;
 using std::vector;
 
+inline double function_root(double f(double x), double ddx_f(double x), double x0, int iterations) {
+    // Find the nearby root of a differentiable function.
+    for (int i = 0; i < iterations; i++) {
+        x0 = x0 - f(x0)/ddx_f(x0);
+    }
+    return x0;
+}
+
 inline int input_positive_integer() {
     
     int n;
