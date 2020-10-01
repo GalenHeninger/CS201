6f68e662e18461dab761e22655ac7cd73780a30f
Galen Heninger <heninger.galen@gmail.com>
2020-09-30 17:35:52 -0800

    The quadratic function now properly returns a pair of float values for the solutions x1 and x2

--- CS201_HW3/quadratic/quadratic.cpp
+++ CS201_HW3/quadratic/quadratic.cpp
@@ -8,21 +8,37 @@
 
 #include <iostream>
 
-float quadratic(float a, float b, float c) {
+std::pair<float, float> quadratic(float a, float b, float c) {
     
     float d = b*b - 4*a*c;
     int num_roots = int(d >= 0.0) + int(d > 0.0);
     
     float x1, x2;
     
+    switch (num_roots) {
+        case 0:
+            // No roots
+            x1 = x2 = NULL;
+            break;
+        case 1:
+            x1 = x2 = -0.5*b/a;
+            break;
+        
+        case 2:
+            x1 = 0.5*(-b - d)/a;
+            x2 = 0.5*(-b + d)/a;
+            break;
+    }
     
-    
-    return x1, x2;
+    return {x1, x2};
     
 }
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    std::pair<float, float> solutions = quadratic(1.0, -2.0, -1.0);
+    
+    
+    
     return 0;
 }
