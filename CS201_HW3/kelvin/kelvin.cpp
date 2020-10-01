3713c03d087a3098c984479240a0b8dc5fafb623
Galen Heninger <heninger.galen@gmail.com>
2020-09-30 16:36:39 -0800

    Entered given "kelvin.cpp" code

---
+++ CS201_HW3/kelvin/kelvin.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  kelvin
+//
+//  Created by Galen on 9/30/20.
+//  Copyright © 2020 Galen. All rights reserved.
+//
+
+#include <iostream>
+
+double ctok(double c)
+{
+    int k = c + 273.15
+    return int;
+}
+
+int main(int argc, const char * argv[]) {
+    
+    double c = 0;
+    cin >> d;
+    double k = ctok("c");
+    Cout << k << '/n';
+    
+    return 0;
+}
