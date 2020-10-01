9033e03b51e6f3053737dbae2bd124631feb8f0c
Galen Heninger <heninger.galen@gmail.com>
2020-09-30 16:54:22 -0800

    Corrected various errors in the given code

--- CS201_HW3/kelvin/kelvin.cpp
+++ CS201_HW3/kelvin/kelvin.cpp
@@ -8,18 +8,27 @@
 
 #include <iostream>
 
-double ctok(double c)
-{
-    int k = c + 273.15
-    return int;
+// Errors:
+// The ctok function does not return the expression k
+// in main, cin >> d should instead be std::cin >> c
+// The variable c cannot be referenced by writing <"c">
+// Cout should be std::cout
+// The new line string /n should be \n instead
+// The string \n should be written with double quotes
+// The name "ctok" is not understandable out of context
+
+
+double celsius_to_kelvin(double c) {
+    int k = c + 273.15;
+    return k;
 }
 
 int main(int argc, const char * argv[]) {
     
     double c = 0;
-    cin >> d;
-    double k = ctok("c");
-    Cout << k << '/n';
+    std::cin >> c;
+    double k = celsius_to_kelvin(c);
+    std::cout << k << "\n";
     
     return 0;
 }
