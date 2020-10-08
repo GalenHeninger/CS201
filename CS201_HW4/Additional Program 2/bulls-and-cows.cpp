10d52e024aa5e03909fadf669dfb80debdf36a15
Galen Heninger <heninger.galen@gmail.com>
2020-10-08 15:59:02 -0800

    Separate function for printing the contents of v

--- CS201_HW4/Additional Program 2/bulls-and-cows.cpp
+++ CS201_HW4/Additional Program 2/bulls-and-cows.cpp
@@ -11,16 +11,23 @@
 
 using namespace std;
 
+void print_numbers(vector<int> v) {
+    for (auto n:v) {
+        cout << n << " ";
+    }
+}
+
 int main(int argc, const char * argv[]) {
     
     vector<int> v(4);
     std::generate(v.begin(), v.end(), std::rand);
     
-    
     for (int i = 0; i < v.size(); i++) {
         v.at(i) = v.at(i) % 10;
-        cout << v.at(i) << " ";
     }
+    
+    
+    
     
     cout << endl;
     
