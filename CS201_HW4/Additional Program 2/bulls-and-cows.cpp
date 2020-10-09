66b699fb89a537e52468c75ae5420d5f0dc35b8c
Galen Heninger <heninger.galen@gmail.com>
2020-10-08 16:20:43 -0800

    The random integers generated now are never equal

--- CS201_HW4/Additional Program 2/bulls-and-cows.cpp
+++ CS201_HW4/Additional Program 2/bulls-and-cows.cpp
@@ -17,19 +17,35 @@ void print_numbers(vector<int> v) {
     }
 }
 
+void initialize_different_random_integers(vector<int> &v) {
+    // generate integers randomly so that none are equal
+    for(int i = 0; i < v.size(); i++) {
+        
+        v.at(i) = rand() % (10 - i); // Range from 0 to 10 - i
+        
+        if(i>0){
+            // Shift numbers so that none can be equal
+            if(v.at(i) >= v.at(i - 1)){
+                v.at(i) = v.at(i) + i;
+            }
+        }
+    }
+}
+
 int main(int argc, const char * argv[]) {
     
     vector<int> v(4);
-    std::generate(v.begin(), v.end(), std::rand);
     
-    for (int i = 0; i < v.size(); i++) {
-        v.at(i) = v.at(i) % 10;
+    for (int i = 0; i < 100; i++) {
+        initialize_different_random_integers(v);
+        print_numbers(v);
+        cout << endl;
     }
     
     
     
     
-    cout << endl;
+    
     
     return 0;
 }
