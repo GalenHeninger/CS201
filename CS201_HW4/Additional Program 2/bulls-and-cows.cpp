88403e9c22df783d88d04b6a10415115de3adc6b
Galen Heninger <heninger.galen@gmail.com>
2020-10-09 17:25:02 -0800

    rand() is now given a seed so that the values differ each time it compiles

--- CS201_HW4/Additional Program 2/bulls-and-cows.cpp
+++ CS201_HW4/Additional Program 2/bulls-and-cows.cpp
@@ -21,10 +21,10 @@ void initialize_different_random_integers(vector<int> &v) {
     // generate integers randomly so that none are equal
     for(int i = 0; i < v.size(); i++) {
         
-        v.at(i) = rand() % (10 - i); // Range from 0 to 10 - i
+        v.at(i) = rand() % (10 - i); // Range from 0 to 9 - i
         
         if(i>0){
-            // Shift numbers so that none can be equal; range 0 to 10
+            // Shift numbers so that none can be equal; range 0 to 9
             if(v.at(i) >= v.at(i - 1)){
                 v.at(i) = v.at(i) + i;
             }
@@ -36,7 +36,7 @@ void guess_input(vector<int> &guess) {
     int n = 0;
     int i = 0;
     while(n >= 0 && i < 4) {
-        cout << "Enter a guess for integer " << i + 1 << ":\n";
+        cout << "Enter a guess for integer " << i + 1 << ": ";
         cin >> n;
         
         guess.at(i) = n;
@@ -70,35 +70,43 @@ int number_correct_with_right_placement(vector<int> v, vector<int> guess) {
     return n;
 }
 
-int main(int argc, const char * argv[]) {
+int main() {
+    
+    // rand should not always generate the same digit
+    srand(time(0));
     
     vector<int> v(4);
     vector<int> guess(4);
     
-    cout << "There are four different digits ranged from 0 to 10.\n";
+    cout << "There are four different digits ranged from 0 to 9.\n";
+    
+    initialize_different_random_integers(v);
+    print_numbers(v);
+    cout << endl;
     
     do {
         
         
-        initialize_different_random_integers(v);
-        print_numbers(v);
-        
         guess_input(guess);
-        print_numbers(v);
+        print_numbers(guess);
+        cout << endl;
         
-        int a = number_correct_with_wrong_placement(v, guess);
+        int a = total_number_correct(v, guess);
         int b = number_correct_with_right_placement(v, guess);
         
-        cout << "There are " << a << " correct numbers with the wrong placement.\n";
-        cout << "There are " << b << " correct numbers with the right placement.\n";
-        
-        cout << endl;
+        if(v != guess) {
+            cout << "There are " << a - b << " correct numbers with the wrong placement.\n";
+            cout << "There are " << b << " correct numbers with the right placement.\n";
+            
+            cout << endl;
+        }
         
     } while (v != guess);
     
     if(v == guess) {
         cout << "Correct.\n";
     }
+    
     cout << endl;
     
     
