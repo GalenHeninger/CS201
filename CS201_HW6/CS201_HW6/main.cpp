e2c3ae021211c03e97ec36aab74b13b2009df923
Galen Heninger <heninger.galen@gmail.com>
2020-11-11 08:03:43 -0900

    Functions for different distributions

---
+++ CS201_HW6/CS201_HW6/main.cpp
@@ -0,0 +1,74 @@
+//
+//  main.cpp
+//  CS201_HW6
+//
+//  Created by Galen on 11/10/20.
+//  Copyright © 2020 Galen. All rights reserved.
+//
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <random>
+#include <cmath>
+
+using std::mt19937;
+
+int RandomBetweenU(int first, int last) {
+    
+    std::random_device device;
+    std::mt19937 generator(device());
+    std::uniform_int_distribution<int> distribution(first, last);
+    
+    return distribution(generator);
+}
+
+int RandomBetweenN(int first, int last) {
+    
+    std::random_device device;
+    std::mt19937 generator(device());
+    std::normal_distribution<> normal_distribution(first, last);
+    
+    return normal_distribution(generator);
+}
+
+int RandomBetween(int first, int last) {
+    return rand() % (last - first + 1) + first;
+}
+
+void PrintDistribution(const std::map<int, int> &numbers) {
+    
+}
+ 
+int main()
+{
+    srand(0);
+    
+    // Seed with a real random value, if available
+    std::random_device r;
+ 
+    // Choose a random mean between 1 and 6
+    std::default_random_engine e1(r());
+    std::uniform_int_distribution<int> uniform_dist(1, 6);
+    int mean = uniform_dist(e1);
+    std::cout << "Randomly-chosen mean: " << mean << '\n';
+ 
+    // Generate a normal distribution around that mean
+    std::seed_seq seed2{r(), r(), r(), r(), r(), r(), r(), r()};
+    std::mt19937 e2(seed2);
+    std::normal_distribution<> normal_dist(mean, 2);
+ 
+    std::map<int, int> hist;
+    for (int n = 0; n < 10000; ++n) {
+        ++hist[std::round(normal_dist(e2))];
+    }
+    
+    PrintDistribution(hist);
+    
+    std::cout << "Normal distribution around " << mean << ":\n";
+    for (auto p : hist) {
+        std::cout << std::fixed << std::setprecision(1) << std::setw(2)
+                  << p.first << ' ' << std::string(p.second/200, '*') << '\n';
+    }
+}
